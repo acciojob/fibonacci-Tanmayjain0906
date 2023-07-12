bd57fd2f254c152837c2bee0bf748dc2d146beb1
Tanmay jain <134151751+Tanmayjain0906@users.noreply.github.com>
2023-07-12 13:07:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function fibonacci(num) {
-// your code here
-}
-
-module.exports = fibonacci;
+    if(num==1)
+      {
+          return 0;
+      }
+       if(num==2)
+       {
+           return 1;
+       }
+       else
+       {
+           return fibonacci(num-1)+fibonacci(num-2);
+       }
+   }
+   
+   let fib = promp4t("enter your number");
+   console.log(fibonacci(fib));
